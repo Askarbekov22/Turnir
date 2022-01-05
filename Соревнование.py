59357bf4030149c6443dc40d3206a9124d66dbf8
Askarbekov22 <96426154+Askarbekov22@users.noreply.github.com>
2022-01-05 23:38:57 +0600

    New

--- Соревнование.py
+++ Соревнование.py
@@ -16,4 +16,58 @@ print(f'Hello, {name}!')
 n = int(input())
 print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
 print('The previous number for the number ' + str(n) + ' is ' + str(n - 1) + '.')
+a = int(input())
+b = int(input())
+c = int(input())
+print(a // 2 + b // 2 + c // 2 + a % 2 + b % 2 + c % 2)
+a = int(input())
+b = int(input())
+L = int(input())
+N = int(input())
+print(2 * L + (2 * N - 1) * a + 2 * (N - 1) * b)
+a = int(input( ))
+b = int(input( ))
+if a>b:
+    print(b)
+elif b>a:
+    print(a)
+else:
+    print(a)
+x = int(input( ))
+if x>0:
+    print(1)
+elif x < 0:
+    print(-1)
+else:
+    print(0)
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if (x1 + y1 + x2 + y2) % 2 == 0:
+    print('YES')
+else:
+    print('NO')
+a = int(input( ))
+b = int(input( ))
+c = int(input( ))
+if a <= b and a<=c:
+    print(a)
+elif b<=a and b<=c:
+    print(b)
+elif c <= a and c <=b:
+    print(c)
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b == c:
+    print(3)
+elif a == b or b == c or a == c:
+    print(2)
+else:
+    print(0)
+A = int(input( ))
+B = int(input( ))
+for i in range(A,B+1):
+        print(i)
 """
