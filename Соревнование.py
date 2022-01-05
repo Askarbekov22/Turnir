c99a3e3cc50a0a615f5be724b1df9af1c97a72be
Askarbekov22 <96426154+Askarbekov22@users.noreply.github.com>
2022-01-05 21:26:09 +0600

    New

---
+++ Соревнование.py
@@ -0,0 +1,19 @@
+#Задание №1
+"""
+a = int(input())
+b = int(input())
+c=int(input())
+print(a + b+c)
+b = int(input())
+h = int(input())
+print (0.5*h*b)
+n = int(input())
+k = int (input ())
+print(k // n)
+print(k - (k // n) * n)
+name=input( )
+print(f'Hello, {name}!')
+n = int(input())
+print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
+print('The previous number for the number ' + str(n) + ' is ' + str(n - 1) + '.')
+"""
